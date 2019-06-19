73fad7b250c4b4b2a76d05e6321f5c763d8df7b3
Ernesto <nadavernesto@protonmail.com>
2019-06-19 13:51:07 -0300

    Modificação para modelo árvore+lista

--- twitter.h
+++ twitter.h
@@ -11,22 +11,31 @@ typedef struct{
 	char texto[141];
 	int curtidas;
 	int retweets;
+	struct lista_tweet *prox;
 }tweet;
 
 typedef struct{
 	struct tweet *dado;
-	struct lista_tweet *prox;
-}lista_tweet;
+	struct arv_tweet dir;
+	struct arv_tweet esq;
+}arv_tweet;
 
 typedef struct{
 	char nome[30];
 	int usos;
-	struct lista_tweet *aparicoes;
-	struct lista_hashtag *associadas;
+	struct relacionadas *associadas;
+	struct hashtag *prox;
 }hashtag;
+
+typedef struct{
+	struct hashtag lista;
+	struct arv_hashtag dir;
+	struct arv_hashtag esq;
+}arv_hashtag;
+	
 
 typedef struct{
 	struct hashtag *dado;
 	int encontros;
-	struct lista_hashtag *prox;
-}lista_hashtag;
+	struct relacionadas *prox;
+}relacionadas;
