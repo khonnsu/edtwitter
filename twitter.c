6e554c2e59373ee763573f58f073509b6f82c8e6
khonnsu <51890424+khonnsu@users.noreply.github.com>
2019-06-19 16:53:32 -0300

    Update twitter.c

--- twitter.c
+++ twitter.c
@@ -1,22 +1,39 @@
 int learquivo(char *nome,usuario *raiz_u, usuario *ini_u, usuario *fim_u, hashtag *raiz_h, hashtag *ini_h, hashtag *fim_h){
-
   FILE *arq;
-  char temp[16];
   
   arq=fopen(nome,"r");
   if(arq==null)
     return -1;
-  else
-  {
-    while(!feof(arq))
-    {
-      atual=malloc(sizeof(tweet));
-      scanf("%[^;]s",temp);
-    }
-    if(verifica_existencia(temp))
-    {
+  else{
+      
+    usuario *user;
+    raiz_u = user;
+    ini_u = user;
+    fim_u = user;
+
+    hashtag *hash;
+    raiz_h = hash;
+    ini_h = hash;
+    fim_h = hash;
     
+    char nick[16];
+    char texto_tweet[141];
+    
+    while(!feof(arq)){
+      atual=malloc(sizeof(tweet));
+      scanf("%[^;]s",nick);
+      user = verifica_exist(nick);
+      if(user == NULL){
+        user=malloc(sizeof(user));
+        user->nickname = nick;
+      }
+      
+      
+          
     }
+ 
+    
+    else
   }
 
 }
