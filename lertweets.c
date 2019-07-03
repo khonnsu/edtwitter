7c0e69886258fe26b6aef3d289469f721ae053d8
Ernesto <nadavernesto@protonmail.com>
2019-07-03 14:54:41 -0300

    Main atualizada com tratamento de erros e chamamento de função.

--- lertweets.c
+++ lertweets.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
-#include <string.h>
+#include "twitter.h"
 
 int main(int argc, char **argv){
+
 	FILE *ent = fopen(arg[1],"r");
+	if(ent==NULL){
+		printf("Erro ao abrir arquivo de entrada\n");		
+		return -1;
+	}
 	FILE *ops = fopen(arg[2],"r");
-	FILE *sai = fopen(arg[3],"w"); 
+	if(ops==NULL){
+		printf("Erro ao abrir arquivo de operacoes\n");
+		return -1;
+	}	
+	FILE *sai = fopen(arg[3],"w");
+	if(sai=NULL){
+		printf("Erro ao criar arquivo de saída\n");
+		return -1;
+	}
+		
+	int opa,opb,opc,opd,ope,opf;
+	char opg;
+
+	leparametros(ops,&opa,&opb,&opc,&opd,&ope,&opf,&opg);
+
+			
+
+	 
